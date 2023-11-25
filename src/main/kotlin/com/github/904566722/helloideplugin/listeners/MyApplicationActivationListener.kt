f1b2e954c69f7df16a238fe9e2351d76695469df
GitHub Action <action@github.com>
2023-11-25 08:13:58 +0000

    Template cleanup

--- src/main/kotlin/com/github/904566722/helloideplugin/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/904566722/helloideplugin/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.904566722.helloideplugin.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
--- src/main/kotlin/com/github/904566722/helloideplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/904566722/helloideplugin/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.904566722.helloideplugin.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.904566722.helloideplugin.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
